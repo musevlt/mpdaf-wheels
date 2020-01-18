24663800658edfc677e1525dc4520b1fd137da45
Simon Conseil <contact@saimon.org>
2020-01-17 23:52:52 -0300

    Remove manual handling of numpy versions
    This should no more be necessary thanks to pyproject.toml

--- build-deps.sh
+++ build-deps.sh
@@ -2,20 +2,6 @@
 
 set -e
 
-echo "# Installing Numpy"
-pyversion=$(python -c 'import sys; print("%d.%d" % (sys.version_info.major, sys.version_info.minor))')
-case "$pyversion" in 
-  3.5)
-    pip install numpy==1.13.3
-    ;;
-  3.6)
-    pip install numpy==1.13.3
-    ;;
-  3.7)
-    pip install numpy==1.14.5
-    ;;
-esac
-
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install cfitsio
     brew info cfitsio
