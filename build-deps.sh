6565712ad33bf1b00512a8dbfa2fb3da8a20e88d
Simon Conseil <contact@saimon.org>
2020-02-20 01:01:33 -0300

    Remove brew

--- build-deps.sh
+++ build-deps.sh
@@ -5,15 +5,9 @@ set -e
 
 pip install -U Cython
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    brew install cfitsio
-    brew info cfitsio
-    echo $(brew --prefix cfitsio)
-else
-    echo "# Downloading cfitsio"
-    curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xz
-    echo "# Installing cfitsio"
-    cd cfitsio
-    ./configure && make clean shared install
-    cd ..
-fi
+echo "# Downloading cfitsio"
+curl -s https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio3370.tar.gz | tar xz
+echo "# Installing cfitsio"
+cd cfitsio
+./configure && make clean shared install
+cd ..
