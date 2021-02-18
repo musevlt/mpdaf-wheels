9ebcc9e381ce948b366e4ed0e5be1d80074a4125
Simon Conseil <contact@saimon.org>
2021-02-18 10:25:48 -0300

    Fix directory

--- build-deps.sh
+++ build-deps.sh
@@ -8,6 +8,6 @@ set -e
 echo "# Downloading cfitsio"
 curl https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.49.tar.gz | tar xz
 echo "# Installing cfitsio"
-cd cfitsio
+cd cfitsio-3.49
 ./configure && make clean shared install
 cd ..
